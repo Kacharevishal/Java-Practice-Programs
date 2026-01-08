9451b48132680a5d293d9a76b0f5c66773deb102
Vishal Kachare <155882477+Kacharevishal@users.noreply.github.com>
2026-01-08 11:35:44 +0530

    Add files via upload

---
+++ GenSeries149.java
@@ -0,0 +1,15 @@
+
+public class GenSeries149 {
+
+	public static void main(String[] args) {
+
+		// Generate The Series 1 4 9 16 25 36 49 64 81 100.
+		
+		for(int i = 1;i<=10;i++)
+		{
+			System.out.print(i*i+" ");
+		}
+		
+	}
+
+}
---
+++ GenSeris369.java
@@ -0,0 +1,14 @@
+
+public class GenSeris369 {
+
+	public static void main(String[] args) {
+
+		// Generate The Series 3 6 9 12 15 18 21 24 27 30.
+
+		for(int i = 1;i<=10;i++)
+		{
+			System.out.print(i*3+" ");
+		}
+	}
+
+}
